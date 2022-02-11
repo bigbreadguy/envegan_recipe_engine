c3b6f4993a3867df956cfda1445258ea5028d05f
bigbreadguy <schadenlim@gmail.com>
2022-02-12 00:11:54 +0900

    extract vegan entity
    estimate vegan entity from review

--- test_analysis.py
+++ test_analysis.py
@@ -5,6 +5,10 @@ import json
 import os
 from tqdm import tqdm
 
+analysis_result = os.path.join(os.getcwd(), "analysis_result")
+if not os.path.exists(analysis_result):
+    os.makedirs(analysis_result)
+
 steak_reviews = os.path.join("review_data", "johnny_prime_steaks.json")
 with open(steak_reviews, "r", encoding="UTF-8-SIG") as f:
     review_data = json.load(f)
@@ -139,27 +143,106 @@ mctfidf = mtfidf[:,mcommon] #mctfidf가 잘못 구해짐
 
 plt.title("reviews over words")
 plt.imshow(rctfidf)
-plt.savefig("rctfidf.png", dpi=300, format="png")
+plt.savefig(os.path.join(analysis_result, "rctfidf.png"), dpi=300, format="png")
 
 plt.title("molecules over words")
 plt.imshow(mctfidf)
-plt.savefig("mctfidf.png", dpi=300, format="png")
+plt.savefig(os.path.join(analysis_result, "mctfidf.png"), dpi=300, format="png")
 
 mc = np.matmul(rctfidf, mctfidf.T, dtype=np.float32)
 
 print(mc.shape)
 
+rm_result = os.path.join(analysis_result, "review-molecule")
+if not os.path.exists(rm_result):
+    os.makedirs(rm_result)
+
 for i in range(N):
     ss = sorted(range(len(mc[i])), key=lambda k: mc[i][k], reverse=True)
-    if np.max(mc[i]) > 0:
-        break
-
-print(review_data[i]["place"])
-
-with open("test_result.txt", "w") as f:
-    for s in ss:
-        f.write("{} : {:.2e}\n".format(list(fdb_meat.keys())[s], mc[i][s]))
+    
+    with open(os.path.join(rm_result, f"{review_data[i]['place']}.txt"), "w") as f:
+        f.write("molecule, intensity\n")
+        for s in ss:
+            f.write("{}, {:.2e}\n".format(list(fdb_meat.keys())[s], mc[i][s]))
 
 plt.title("reviews over molecules")
 plt.imshow(mc)
-plt.savefig("test_result.png", dpi=300, format="png")
+plt.savefig(os.path.join(analysis_result, "test_result.png"), dpi=300, format="png")
+
+flavor_db = os.path.join(os.getcwd(), "flavor_db")
+entities = os.listdir(flavor_db)
+
+categories = set([])
+
+data_dicts = []
+
+for entity in entities:
+    with open(os.path.join(flavor_db, entity)) as f:
+        data_dict = json.load(f)
+        data_dicts.append(data_dict)
+        
+    categories.add(data_dict["category_readable"])
+
+vegan_categories = ('Spice', 'Plant Derivative', 'Cereal', 'Seed', 'Legume',
+                    'Vegetable', 'Fruit', 'Vegetable Root', 'Maize', 'Vegetable Fruit',
+                    'Berry', 'Vegetable Stem', 'Nut', 'Fruit-Berry','Vegetable Tuber',
+                    'Herb', 'Fungus', 'Essential Oil', 'Fruit Essence', 'Cabbage',
+                    'Fruit Citrus', 'Plant', 'Flower')
+
+vegan_entities = []
+
+for entity in data_dicts:
+    if entity["category_readable"] in vegan_categories:
+        print(entity["entity_alias_readable"])
+        vegan_entities.append(entity)
+
+fdb_veg = {}
+veg_profiles = set([])
+
+for entity in vegan_entities:
+    name = entity["entity_alias_readable"]
+    
+    profiles = []
+    for molecule in entity["molecules"]:
+        profiles.extend(molecule["flavor_profile"].split("@"))
+    
+    fdb_veg[name] = profiles
+    veg_profiles.update(profiles)
+
+prof2int = {}
+for i, prof in enumerate(veg_profiles):
+    prof2int[prof] = i
+
+vdoc = np.zeros((len(fdb_veg), len(veg_profiles)), dtype=np.float32)
+veg2int = {}
+
+for i, name, profiles in enumerate(fdb_veg.items()):
+    veg2int[name] = i
+
+    for prof in profiles:
+        j = prof2int[prof]
+    
+    vdoc[i, j] += 1
+
+vtfidf = vdoc[:]
+for i in range(len(fdb_veg)):
+    vtf = np.sum(vtfidf[i, :], dtype=np.float32)
+    vtfidf[i, :] /= vtf
+
+vcommon = [prof2int[w] for w in commonWords]
+
+vctfidf = vtfidf[:, vcommon]
+
+rv = np.matmul(rctfidf, vctfidf.T, dtype=np.float32)
+
+rv_result = os.path.join(analysis_result, "review-vegan")
+if not os.path.exists(rv_result):
+    os.makedirs(rv_result)
+
+for i in range(N):
+    ss1 = sorted(range(len(rv[i])), key=lambda k: rv[i][k], reverse=True)
+    
+    with open(os.path.join(rv_result, f"{review_data[i]['place']}.txt"), "w") as f:
+        f.write("vegan_entity, intensity\n")
+        for s in ss:
+            f.write("{}, {:.2e}\n".format(list(fdb_veg.keys())[s], rv[i][s]))
